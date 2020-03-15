385e786a3852514d441ef922e295adee3e4b2fd7
brayooh <brianndichu.bn@gmail.com>
2020-03-15 23:57:38 +0300

    commit creates url.py file

---
+++ Awards/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, re_path, include
+from . import views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+ 
+
+
+
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
